6f529b99ade1202f64b6169b879db361cf1d9587
Ramansh Saxena <ramansh8055@gmail.com>
2025-05-24 19:59:31 +0530

    first commit

--- client/src/pages/OTPInput.jsx
+++ client/src/pages/OTPInput.jsx
@@ -1,53 +1,54 @@
-import React, { useRef, useEffect } from "react";
-import { TextField, Box } from "@mui/material";
+
+import { useRef, useEffect } from "react"
+import { TextField, Box } from "@mui/material"
 
 const OTPInput = ({ value, onChange, length = 6, autoFocus = false }) => {
-  const inputs = useRef([]);
+  const inputs = useRef([])
 
   useEffect(() => {
     if (autoFocus && inputs.current[0]) {
-      inputs.current[0].focus();
+      inputs.current[0].focus()
     }
-  }, [autoFocus]);
+  }, [autoFocus])
 
   const handleChange = (e, index) => {
-    const val = e.target.value.replace(/\D/g, "");
+    const val = e.target.value.replace(/\D/g, "")
 
     // Paste handling
     if (val.length === length) {
-      onChange(val);
-      inputs.current[length - 1]?.focus();
-      return;
+      onChange(val)
+      inputs.current[length - 1]?.focus()
+      return
     }
 
-    const newValue = value.split("");
-    newValue[index] = val[0] || "";
-    onChange(newValue.join(""));
+    const newValue = value.split("")
+    newValue[index] = val[0] || ""
+    onChange(newValue.join(""))
 
     // Smooth transition to next input
     if (val && index < length - 1) {
       setTimeout(() => {
-        inputs.current[index + 1]?.focus();
-      }, 50);
+        inputs.current[index + 1]?.focus()
+      }, 50)
     }
-  };
+  }
 
   const handleKeyDown = (e, index) => {
     if (e.key === "Backspace") {
-      const newValue = value.split("");
+      const newValue = value.split("")
       if (!newValue[index]) {
         if (index > 0) {
-          inputs.current[index - 1]?.focus();
+          inputs.current[index - 1]?.focus()
         }
       }
-      newValue[index] = "";
-      onChange(newValue.join(""));
+      newValue[index] = ""
+      onChange(newValue.join(""))
     } else if (e.key === "ArrowLeft" && index > 0) {
-      inputs.current[index - 1]?.focus();
+      inputs.current[index - 1]?.focus()
     } else if (e.key === "ArrowRight" && index < length - 1) {
-      inputs.current[index + 1]?.focus();
+      inputs.current[index + 1]?.focus()
     }
-  };
+  }
 
   return (
     <Box display="flex" gap={1} justifyContent="center">
@@ -75,7 +76,7 @@ const OTPInput = ({ value, onChange, length = 6, autoFocus = false }) => {
         />
       ))}
     </Box>
-  );
-};
+  )
+}
 
-export default OTPInput;
+export default OTPInput
